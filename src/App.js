494683bd827f8920f863e0cc33794d72c00a3197
Riya-git257 <r13508503@gmail.com>
2026-03-07 08:52:37 +0530

    Certification update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Hero from './components/Hero'
 import About from './components/About'
 import Skills from './components/Skills'
-import Certifications from './components/Certificates'
+import Certificates from './components/Certificates'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Navbar from './components/Navbar'
@@ -13,7 +13,7 @@ function App()
       <section id="home"><Hero /></section>
       <section id="about"><About /></section>
       <section id="skills"><Skills /></section>
-      <section id="certifications"><Certifications /></section>
+      <section id="certifications"><Certificates /></section>
       <section id="projects"><Projects/></section>
       <section id="contact"><Contact /></section>
     </div>
